1cea4c1e45cc98e0041a2ff7452d382336c68e74
piyumi_rameshka <rameshkafox@gmail.com>
2017-10-09 17:44:28 +0530

    adding query class

--- src/main/java/com/wso2/StrQuery.java
+++ src/main/java/com/wso2/StrQuery.java
@@ -1,4 +1,27 @@
 package com.wso2;
 
 public class StrQuery {
+    String query;
+    String strategy;
+
+    public StrQuery(String query, String strategy) {
+        this.query = query;
+        this.strategy = strategy;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
+    public String getStrategy() {
+        return strategy;
+    }
+
+    public void setStrategy(String strategy) {
+        this.strategy = strategy;
+    }
 }
