ffed857383b9afd272f534eeed44a0fa8890799a
piyumi_rameshka <rameshkafox@gmail.com>
2017-10-10 16:19:47 +0530

    changing json format

--- src/main/java/com/wso2/CreateJson.java
+++ src/main/java/com/wso2/CreateJson.java
@@ -3,38 +3,58 @@ package com.wso2;
 import net.minidev.json.JSONArray;
 import org.json.simple.JSONObject;
 
-
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CreateJson {
 
 
-
-    public void writeConfiguration(List<StrSiddhiApp> strSiddhiAppList,String deploymentJSONURI) throws IOException {
-
-
+    public void writeConfiguration(List<StrSiddhiApp> strSiddhiAppList, String deploymentJSONURI) throws IOException {
 
 
         JSONArray jsonArray = new JSONArray();
         JSONObject jsonObject;
+        HashMap<String,String>  hashMap = new HashMap<String, String>();
 
-        for (StrSiddhiApp strSiddhiApp:strSiddhiAppList){
+        for (StrSiddhiApp strSiddhiApp : strSiddhiAppList) {
             jsonObject = new JSONObject();
-            jsonObject.put("name",strSiddhiApp.getAppName());
-            jsonObject.put("parallel",strSiddhiApp.getParallel());
-            jsonObject.put("app",strSiddhiApp.toJsonString());
+            jsonObject.put("name", strSiddhiApp.getAppName());
+            jsonObject.put("parallel", strSiddhiApp.getParallel());
+            jsonObject.put("app", strSiddhiApp.toJsonString());
+
+            JSONArray jsonArray1 = new JSONArray();
+
+
+            for (Map.Entry<String,StrStream> entry:  strSiddhiApp.getInputStreamMap().entrySet()){
+
+                hashMap = new HashMap<String, String>();
+                hashMap.put(entry.getKey(),entry.getValue().getConsumptionStrategy());
+                jsonArray1.add((hashMap));
+            }
+
+            jsonObject.put("inputStream",jsonArray1);
+
+            JSONArray jsonArray2 = new JSONArray();
+
+            for (Map.Entry<String,StrStream> entry:  strSiddhiApp.getOutputStreamMap().entrySet()){
+                hashMap = new HashMap<String, String>();
+                hashMap.put(entry.getKey(),entry.getValue().getType());
+                jsonArray2.add((hashMap));
+            }
+            jsonObject.put("outputStream",jsonArray2);
 
             jsonArray.add(jsonObject);
 
         }
 
         jsonObject = new JSONObject();
-        jsonObject.put("name","SiddhiApp");
-        jsonObject.put("siddhiApps",jsonArray);
+        jsonObject.put("name", "SiddhiApp");
+        jsonObject.put("siddhiApps", jsonArray);
 
-        String removeEscape = jsonObject.toJSONString().replaceAll("\\\\","");
+        String removeEscape = jsonObject.toJSONString().replaceAll("\\\\", "");
 
         FileWriter fileWriter = new FileWriter(deploymentJSONURI);
         fileWriter.write(removeEscape);
