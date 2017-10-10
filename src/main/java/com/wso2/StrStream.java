2cc85b96062a9c1902f1f2cd101f3eb6cf1b3b1e
piyumi_rameshka <rameshkafox@gmail.com>
2017-10-10 16:21:49 +0530

    adding attributes

--- src/main/java/com/wso2/StrStream.java
+++ src/main/java/com/wso2/StrStream.java
@@ -3,14 +3,32 @@ package com.wso2;
 public class StrStream {
     String type;
     String definition;
+    String consumptionStrategy;
 
     public StrStream(String type, String definition) {
         this.type = type;
         this.definition = definition;
+
+    }
+
+    public StrStream(String type, String definition,String consumptionStrategy) {
+        this.type = type;
+        this.definition = definition;
+        this.consumptionStrategy= consumptionStrategy;
+
+    }
+
+    public String getConsumptionStrategy() {
+        return consumptionStrategy;
+    }
+
+    public void setConsumptionStrategy(String consumptionStrategy) {
+        this.consumptionStrategy = consumptionStrategy;
     }
 
     public String getType() {
         return type;
+
     }
 
     public void setType(String type) {
