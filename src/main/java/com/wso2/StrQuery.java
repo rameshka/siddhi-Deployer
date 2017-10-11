8a560260f7e8eff1e351f8f298c3d3f63dfb29f4
piyumi_rameshka <rameshkafox@gmail.com>
2017-10-11 17:42:39 +0530

    removing strategy

--- src/main/java/com/wso2/StrQuery.java
+++ src/main/java/com/wso2/StrQuery.java
@@ -1,29 +1,18 @@
 package com.wso2;
 
-import java.util.HashMap;
-import java.util.Map;
 
 public class StrQuery {
     String query;
     String strategy;
-    private Map<String, StrStream> inputStreamMap;
-    private Map<String, StrStream> outputStreamMap;
+
 
     public StrQuery(String query, String strategy) {
         this.query = query;
         this.strategy = strategy;
-        inputStreamMap = new HashMap<String, StrStream>();
     }
 
-    public void addInputStream(String key, StrStream strStream) {
-        inputStreamMap.put(key, strStream);
-
-    }
-
-    public void addOutputStream(String key, StrStream strStream) {
-
-        outputStreamMap.put(key, strStream);
-
+    public StrQuery(String query) {
+        this.query = query;
     }
 
     public String getQuery() {
@@ -34,21 +23,6 @@ public class StrQuery {
         this.query = query;
     }
 
-    public Map<String, StrStream> getInputStreamMap() {
-        return inputStreamMap;
-    }
-
-    public void setInputStreamMap(Map<String, StrStream> inputStreamMap) {
-        this.inputStreamMap = inputStreamMap;
-    }
-
-    public Map<String, StrStream> getOutputStreamMap() {
-        return outputStreamMap;
-    }
-
-    public void setOutputStreamMap(Map<String, StrStream> outputStreamMap) {
-        this.outputStreamMap = outputStreamMap;
-    }
 
     public String getStrategy() {
         return strategy;
